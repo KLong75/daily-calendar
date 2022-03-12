4daecdb8b8f120afacb8988cd7244ee48135419b
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-03-12 15:02:15 -0600

    capture clicks on save buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,60 @@
 var currentTime = moment().format("dddd, MMMM Do YYYY");
 document.getElementById("currentDay").append(currentTime);
 
-
+$(document).ready(function () {
+    $("#save-event-7am").click(function() {
+        var txt = $("#event-text-7am").val();
+        $("#event-text-7am").text(txt);
+        console.log(txt);
+    })
+    $("#save-event-8am").click(function() {
+        var txt = $("#event-text-8am").val();
+        console.log(txt);
+    })
+    $("#save-event-9am").click(function() {
+        var txt = $("#event-text-9am").val();
+        console.log(txt);
+    })
+    $("#save-event-10am").click(function() {
+        var txt = $("#event-text-10am").val();
+        console.log(txt);
+    })
+    $("#save-event-11am").click(function() {
+        var txt = $("#event-text-11am").val();
+        console.log(txt);
+    })
+    $("#save-event-12pm").click(function() {
+        var txt = $("#event-text-12pm").val();
+        console.log(txt);
+    })
+    $("#save-event-1pm").click(function() {
+        var txt = $("#event-text-1pm").val();
+        console.log(txt);
+    })
+    $("#save-event-2pm").click(function() {
+        var txt = $("#event-text-2pm").val();
+        console.log(txt);
+    })
+    $("#save-event-3pm").click(function() {
+        var txt = $("#event-text-3pm").val();
+        console.log(txt);
+    })
+    $("#save-event-4pm").click(function() {
+        var txt = $("#event-text-4pm").val();
+        console.log(txt);
+    })
+    $("#save-event-5pm").click(function() {
+        var txt = $("#event-text-5pm").val();
+        console.log(txt);
+    })
+    $("#save-event-6pm").click(function() {
+        var txt = $("#event-text-6pm").val();
+        console.log(txt);
+    })
+    $("#save-event-7pm").click(function() {
+        var txt = $("#event-text-7pm").val();
+        console.log(txt);
+    })
+})
+    
 
